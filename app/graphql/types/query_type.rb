f3b448cfd265e22a610d8094973bfb8d1f16ab4d
Paul Ryan <pryan@chime.com>
2022-09-20 11:55:15 -0700

    added single author query.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -14,5 +14,14 @@ module Types
       Author.all
     end
 
+    # /author/id
+    field :author, Types::AuthorType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def author(id:)
+      Author.find(id)
+    end
+
   end
 end
