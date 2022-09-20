b6ed3ae363985eedb7c134de959712513151883b
Paul Ryan <pryan@chime.com>
2022-09-20 11:43:32 -0700

    adding chapter to graphql

--- app/graphql/types/chapter_type.rb
+++ app/graphql/types/chapter_type.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 module Types
-  class BookType < Types::BaseObject
+  class ChapterType < Types::BaseObject
     field :id, ID, null: false
     field :name, String
-    field :description, String
-    field :author_id, Integer, null: false
-    field :chapters, [Types::ChapterType], null: false
+    field :short_description, String
+    field :book_id, Integer, null: false
+    field :author_name, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def author_name
+      object.book.author.name
+    end
   end
 end
