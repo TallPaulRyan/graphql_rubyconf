1d8f3455ccb0eeb29d3ca96cfcc14adc7da4740e
Paul Ryan <pryan@chime.com>
2022-09-20 11:34:10 -0700

    adding book to graphql

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
 module Types
-  class AuthorType < Types::BaseObject
+  class BookType < Types::BaseObject
     field :id, ID, null: false
-    field :email, String
     field :name, String
-    field :book_count, Integer
-    field :books, [Types::BookType], null: false
+    field :description, String
+    field :author_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-  
-    def book_count
-      object.books.count
-    end
-  
   end
 end
